2d48d8c64a5e947ea0d835ca1cddc77092dc7eff
Christian <christianlydemann@gmail.com>
2014-11-13 23:27:26 +0100

    upgraded

--- showbiolex.cpp
+++ showbiolex.cpp
@@ -1,6 +1,8 @@
 #include "showbiolex.h"
 #include "ui_showbiolex.h"
+#include <QDebug>
 #include <QTextBrowser>
+
 
 
 
@@ -21,10 +23,10 @@ ShowBiolex::~ShowBiolex()
 {
     delete ui;
 }
-
+// you should be able to apply recommended settings for each plant with a click
 void ShowBiolex::on_pushButton_clicked() // read tomato wiki
 {
-
+    plantSwitch = 1;
     QString tomatText;
     tomatText = "The tomato is the edible, often red fruit/berry of the nightshade Solanum lycopersicum,[1][2] commonly known as a tomato plant. The species originated in the South American Andes[2] and its use as a food originated in Mexico, and spread throughout the world following the Spanish colonization of the Americas. Its many varieties are now widely grown, sometimes in greenhouses in cooler climates.";
 
@@ -38,6 +40,7 @@ void ShowBiolex::on_pushButton_clicked() // read tomato wiki
 
 void ShowBiolex::on_pushButton_2_clicked()
 {
+    plantSwitch = 2;
     QString cucumberText;
     cucumberText = "Cucumber (Cucumis sativus) is a widely cultivated plant in the gourd family Cucurbitaceae. It is a creeping vine that bears cylindrical fruits that are used as culinary vegetables. There are three main varieties of cucumber: slicing, pickling, and burpless. Within these varieties, several different cultivars have emerged. The cucumber is originally from Southern Asia, but now grows on most continents. Many different varieties are traded on the global market.";
 
@@ -47,6 +50,7 @@ void ShowBiolex::on_pushButton_2_clicked()
 
 void ShowBiolex::on_pushButton_3_clicked()
 {
+    plantSwitch = 3;
     QString grape;
     grape = "A grape is a fruiting berry of the deciduous woody vines of the botanical genus Vitis. Grapes can be eaten raw or they can be used for making wine, jam, juice, jelly, grape seed extract, raisins, vinegar, and grape seed oil. Grapes are a non-climacteric type of fruit, generally occurring in clusters.";
     ui->textBrowser->setText(grape);
@@ -55,4 +59,50 @@ void ShowBiolex::on_pushButton_3_clicked()
 void ShowBiolex::on_pushButton_4_clicked()
 {
     this->close();
+}
+
+void ShowBiolex::on_recbutton_clicked()
+{
+    // create a list of recommendations for the values and set them to the changeAuto settings.
+    recommendationsTomato = "Recommendations for tomato: humidity: 20 %, min: 20 c, max 30 c";
+
+
+
+    qDebug() << "plant switch: " << plantSwitch;
+    ui->recbutton->setText("Apply Recommendations");
+    ui->recbutton->setSizeIncrement(3,0);
+    switch(plantSwitch)// switchcase with the different events
+    {
+        case 1:
+
+        ui->textBrowser->setText(recommendationsTomato);
+
+
+
+           //ui->recbutton->click();// apply recommendations on click.
+
+    {
+        //ptr_currentData->setTargetHumidity(20);
+        //uiauto->humidityLcd->display(ptr_currentData->getTargetHumidity());
+        //ptr_currentData->setMinTemp(20);
+        //uiauto->tempLcd->display(ptr_currentData->getMinTemp());
+        //ptr_currentData->setMaxTemp(30);
+       // uiauto->tempLcd->display(ptr_currentData->getMaxTemp());
+
+    }
+            break;
+        case 2: //cucumber
+
+        break;
+
+        case 3: // grape
+
+        break;
+    default:
+        return;
+
+    }
+
+
+
 }
--- showbiolex.h
+++ showbiolex.h
@@ -1,6 +1,9 @@
 #ifndef SHOWBIOLEX_H
 #define SHOWBIOLEX_H
+#include "changeauto.h"
+#include "ui_changeauto.h"
 
+#include "currentdata.h"
 #include <QDialog>
 
 namespace Ui {
@@ -24,8 +27,18 @@ private slots:
 
     void on_pushButton_4_clicked();
 
+    void on_recbutton_clicked();
+
+
+
 private:
     Ui::ShowBiolex *ui;
+    Ui::ChangeAuto *uiauto;
+    int plantSwitch; // for switch
+    CurrentData *ptr_currentData;
+    QString recommendationsTomato;
+    QString recommendationsCucumber;
+    QString recommendationsGrape;
 };
 
 #endif // SHOWBIOLEX_H
